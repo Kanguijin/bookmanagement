387692da9285071c29f2a7528c7d0434ab458c98
Kanguijin <wheretogobro@naver.com>
2023-04-19 23:23:48 +0900

    Authority 완료

--- src/main/java/com/toyproject/bookmanagement/service/AuthenticationService.java
+++ src/main/java/com/toyproject/bookmanagement/service/AuthenticationService.java
@@ -1,8 +1,12 @@
 package com.toyproject.bookmanagement.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.toyproject.bookmanagement.dto.auth.SignupReqDto;
+import com.toyproject.bookmanagement.entity.Authority;
 import com.toyproject.bookmanagement.entity.User;
 import com.toyproject.bookmanagement.exception.CustomException;
 import com.toyproject.bookmanagement.exception.ErrorMap;
@@ -28,5 +32,15 @@ public class AuthenticationService {
 	public void signup(SignupReqDto signupReqDto) {
 		User userEntity = signupReqDto.toEntity();
 		userRepository.saveUser(userEntity);
+		
+		List<Authority> authorities = new ArrayList<>();
+		authorities.add(Authority.builder()
+						.userId(userEntity.getUserId())
+						.roleId(1)
+						.build());
+		System.out.println(authorities);
+		userRepository.saveAuthority(authorities);
 	}
+	
+	
 }
