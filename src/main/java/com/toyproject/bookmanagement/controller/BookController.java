f90f02e286cc535653618226d35ada7a617dd59d
Kanguijin <wheretogobro@naver.com>
2023-04-27 17:45:48 +0900

    대여 기능 완료

--- src/main/java/com/toyproject/bookmanagement/controller/BookController.java
+++ src/main/java/com/toyproject/bookmanagement/controller/BookController.java
@@ -60,4 +60,13 @@ public class BookController {
 	public ResponseEntity<?> getRentalListByBookId(@PathVariable int bookId) {
 		return ResponseEntity.ok().body(bookService.getRentalListByBookId(bookId));
 	}
+	
+	@PostMapping("/book/rental/{bookListId}")
+	public ResponseEntity<?> rentalBook(@PathVariable int bookListId, @RequestBody Map<String, Integer> requestMap) {
+		return ResponseEntity.ok().body(bookService.rentalBook(bookListId, requestMap.get("userId")));
+	}
+	@DeleteMapping("/book/rental/{bookListId}")
+	public ResponseEntity<?> returnBook(@PathVariable int bookListId, int userId) {
+		return ResponseEntity.ok().body(bookService.returnBook(bookListId, userId));
+	}
 }
